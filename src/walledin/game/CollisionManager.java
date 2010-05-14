063eb9e7b18f4c86322dca19f3085390f40a3e16
Ben Ruijl <benruyl@gmail.com>
2010-05-14 19:51:06 +0200

    Clean up of collision code

--- src/walledin/game/CollisionManager.java
+++ src/walledin/game/CollisionManager.java
@@ -11,9 +11,6 @@ import walledin.game.map.GameMap;
 import walledin.game.map.Tile;
 
 public class CollisionManager {
-	public CollisionManager() {
-		// TODO Auto-generated constructor stub
-	}
 
 	private Tile tileFromPixel(GameMap map, Vector2f pos) {
 		float tileSize = map.getAttribute(Attribute.RENDER_TILE_SIZE);
@@ -32,62 +29,61 @@ public class CollisionManager {
 		for (Entity ent : entities)
 			if (ent.hasAttribute(Attribute.BOUNDING_BOX)
 					&& !ent.hasAttribute(Attribute.TILES)) {
-				Rectangle rect2 = ent.getAttribute(Attribute.BOUNDING_BOX);
-				Vector2f vel2 = ent.getAttribute(Attribute.VELOCITY);
-				vel2 = vel2.scale((float) delta); // vel per frame
-
-				if (vel2.x == 0 && vel2.y == 0) // deterministic float
-												// comparison?
+				
+				Vector2f vel = ent.getAttribute(Attribute.VELOCITY);
+				
+				if (vel.x == 0 && vel.y == 0)
 					continue;
 
-				Vector2f endpos2 = ent.getAttribute(Attribute.POSITION);
-				Vector2f oldpos2 = endpos2.sub(vel2);
+				vel = vel.scale((float) delta); // velocity per frame
+				Rectangle rect = ent.getAttribute(Attribute.BOUNDING_BOX);
+				Vector2f curPos = ent.getAttribute(Attribute.POSITION);
+				Vector2f oldPos = curPos.sub(vel);
 
-				float x = endpos2.x;
-				float y = endpos2.y;
-				float eps = 0.001f;
+				float x = curPos.x; // new x position after collision
+				float y = curPos.y; // new y position after collision
+				float eps = 0.001f; // small value to prevent floating errors
 
 				// VERTICAL CHECK - move vertically only
-				rect2 = rect2
-						.setPos(new Vector2f(oldpos2.x, oldpos2.y + vel2.y));
+				rect = rect.setPos(new Vector2f(oldPos.x, curPos.y));
 
 				// check the four edges
-				Tile lt = tileFromPixel(map, rect2.getLeftTop());
-				Tile lb = tileFromPixel(map, rect2.getLeftBottom());
-				Tile rt = tileFromPixel(map, rect2.getRightTop());
-				Tile rb = tileFromPixel(map, rect2.getRightBottom());
+				Tile lt = tileFromPixel(map, rect.getLeftTop());
+				Tile lb = tileFromPixel(map, rect.getLeftBottom());
+				Tile rt = tileFromPixel(map, rect.getRightTop());
+				Tile rb = tileFromPixel(map, rect.getRightBottom());
 
 				// bottom check
-				if (vel2.y > 0
+				if (vel.y > 0
 						&& (lb.getType().isSolid() || rb.getType().isSolid())) {
-					int rest = (int) (rect2.getBottom() / tileSize);
-					y = rest * tileSize - rect2.getHeight() - eps;
+					int rest = (int) (rect.getBottom() / tileSize);
+					y = rest * tileSize - rect.getHeight() - eps;
 				} else
 				// top check
-				if (vel2.y < 0
+				if (vel.y < 0
 						&& (lt.getType().isSolid() || rt.getType().isSolid())) {
-					int rest = (int) (rect2.getTop() / tileSize);
+					int rest = (int) (rect.getTop() / tileSize);
 					y = (rest + 1) * tileSize + eps;
 				}
 
 				// HORIZONTAL CHECK - move horizontally only
-				rect2 = rect2.setPos(new Vector2f(oldpos2.x + vel2.x, y)); // new
-																			// position
-				lt = tileFromPixel(map, rect2.getLeftTop());
-				lb = tileFromPixel(map, rect2.getLeftBottom());
-				rt = tileFromPixel(map, rect2.getRightTop());
-				rb = tileFromPixel(map, rect2.getRightBottom());
+				rect = rect.setPos(new Vector2f(curPos.x, y));
+				
+				lt = tileFromPixel(map, rect.getLeftTop());
+				lb = tileFromPixel(map, rect.getLeftBottom());
+				rt = tileFromPixel(map, rect.getRightTop());
+				rb = tileFromPixel(map, rect.getRightBottom());
 
 				// right check
-				if (vel2.x > 0
+				if (vel.x > 0
 						&& (rt.getType().isSolid() || rb.getType().isSolid())) {
-					int rest = (int) (rect2.getRight() / tileSize);
-					x = rest * tileSize - rect2.getWidth() - eps;
+					int rest = (int) (rect.getRight() / tileSize);
+					x = rest * tileSize - rect.getWidth() - eps;
 				} else
 				// left check
-				if (vel2.x < 0
+				if (vel.x < 0
 						&& (lt.getType().isSolid() || lb.getType().isSolid())) {
-					int rest = (int) (rect2.getLeft() / tileSize);
+					int rest = (int) (rect.getLeft() / tileSize);
 					x = (rest + 1) * tileSize + eps;
 				}
 
