af097bc41d2c4e99708dcf76b3ec6fe381399bc6
Wouter Smeenk <woutersmeenk@gmail.com>
2010-05-20 17:32:57 +0200

    newEntity method

--- src/walledin/network/Server.java
+++ src/walledin/network/Server.java
@@ -102,15 +102,21 @@ public class Server {
 
 	private void removePlayer(SocketAddress address) {
 		Player player = players.get(address);
-		entities.remove(player.getName());
+		newPlayers.remove(player);
+		removeEntity(player.getName());
 	}
 
 	private void createPlayer(String name, SocketAddress address) {
 		Player player = new Player(name);
-		entities.put(name, player);
+		newEntity(player);
+		newPlayers.add(player);
 		player.setAttribute(Attribute.POSITION,
 				new Vector2f(400, 300));
 		players.put(address, player);
+	}
+
+	private void newEntity(Entity entity) {
+		entities.put(entity.getName(), entity);
 	}
 
 	public void update(final double delta) {
@@ -142,13 +148,13 @@ public class Server {
 
 		final GameMapIO mMapIO = new GameMapIOXML(); // choose XML as format
 
-		entities.put("Map", mMapIO.readFromFile("data/map.xml"));
+		newEntity(mMapIO.readFromFile("data/map.xml"));
 
 		// add map items like healthkits to entity list
 		final List<Item> mapItems = entities.get("Map").getAttribute(
 				Attribute.ITEM_LIST);
 		for (final Item item : mapItems) {
-			entities.put(item.getName(), item);
+			newEntity(item);
 		}
 	}
 
