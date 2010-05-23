ab2b9dacb60a540367d043c3b4fb2c4b0e5aacf0
Wouter Smeenk <woutersmeenk@gmail.com>
2010-05-23 22:08:00 +0200

    copyleft license

--- src/walledin/game/ZValues.java
+++ src/walledin/game/ZValues.java
@@ -1,3 +1,23 @@
+/*  Copyright 2010 Ben Ruijl, Wouter Smeenk
+
+This file is part of Walled In.
+
+Walled In is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+Walled In is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Walled In; see the file LICENSE.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+*/
 package walledin.game;
 
 public enum ZValues {
