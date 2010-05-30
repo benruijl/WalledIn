79615e4bc7d18dde04a004e81fd949a5862a1862
Wouter Smeenk <woutersmeenk@gmail.com>
2010-05-30 18:05:16 +0200

    fixed handgun bug. a uniq name is now generated

--- src/main/java/walledin/game/network/server/Server.java
+++ src/main/java/walledin/game/network/server/Server.java
@@ -307,7 +307,7 @@ public class Server {
 		player.setAttribute(Attribute.PLAYER_NAME, name);
 		
 		/* Let the player start with a handgun */
-		Entity weapon = entityManager.create("Handgun", "hg01");
+		Entity weapon = entityManager.create("Handgun", entityManager.generateUniqueName("Handgun"));
 		player.setAttribute(Attribute.WEAPON, weapon);
 
 		final PlayerConnection con = new PlayerConnection(address, player);
