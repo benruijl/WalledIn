ba34de4c671a5199f4894c053a652a9762c88c86
Wouter Smeenk <woutersmeenk@gmail.com>
2010-07-08 00:59:49 +0200

    fix build and startup scripts

--- dist/client32.bat
+++ dist/client32.bat
@@ -1,1 +1,1 @@
-java -classpath WalledInJava-0.1.jar;lib/*  -Djava.library.path=lib/native/windows32 walledin.game.network.client.Client 
+java -classpath *;lib/*  -Djava.library.path=lib/native/windows32 walledin.game.network.client.Client 
--- dist/client32.sh
+++ dist/client32.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -classpath WalledInJava-0.1.jar:lib/*  -Djava.library.path=lib/native/linux32 walledin.game.network.client.Client 
+java -classpath *:lib/*  -Djava.library.path=lib/native/linux32 walledin.game.network.client.Client 
