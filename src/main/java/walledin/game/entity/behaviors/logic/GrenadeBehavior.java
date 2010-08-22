078ce057a918a5e7bfdee2fae693363ccb37299e
Ben Ruijl <benruyl@gmail.com>
2010-08-22 23:04:10 +0200

    Imporved grenade system

--- src/main/java/walledin/game/entity/behaviors/logic/GrenadeBehavior.java
+++ src/main/java/walledin/game/entity/behaviors/logic/GrenadeBehavior.java
@@ -10,15 +10,15 @@ import walledin.game.entity.MessageType;
 
 public class GrenadeBehavior extends Behavior {
     /** Explode time in seconds. */
-    private final double explodeTime = 3.0;
+    private final double explodeTime = 2.0;
     /**
      * The number of particles created from the explosion. They will fly in
      * different directions.
      */
-    private final int numberOfParticles = 4;
+    private final int numberOfParticles = 5;
     private double time = 0;
-    private Vector2f particleTarget = new Vector2f(0, 100.0f);
-    private Vector2f particleAcc = new Vector2f(0, 30000.0f);
+    private Vector2f particleTarget = new Vector2f(100.0f, 0);
+    private Vector2f particleAcc = new Vector2f(30000.0f, 0);
 
     public GrenadeBehavior(Entity owner) {
         super(owner);
@@ -26,8 +26,6 @@ public class GrenadeBehavior extends Behavior {
 
     @Override
     public void onMessage(MessageType messageType, Object data) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
@@ -43,15 +41,16 @@ public class GrenadeBehavior extends Behavior {
                 foamBullet.setAttribute(Attribute.POSITION,
                         getAttribute(Attribute.POSITION));
 
-                foamBullet.sendMessage(MessageType.APPLY_FORCE,
-                new Matrix2f(i
+                foamBullet.sendMessage(MessageType.APPLY_FORCE, new Matrix2f(-i
                         * Math.PI / (double) numberOfParticles)
                         .apply(particleAcc));
 
-                foamBullet.setAttribute(Attribute.TARGET, new Matrix2f(i
-                        * Math.PI / (double) numberOfParticles)
-                        .apply(particleTarget));
-
+                foamBullet
+                        .setAttribute(Attribute.TARGET, ((Vector2f) foamBullet
+                                .getAttribute(Attribute.POSITION))
+                                .add(new Matrix2f(-i * Math.PI
+                                        / (double) numberOfParticles)
+                                        .apply(particleTarget)));
             }
 
             getOwner().remove();
